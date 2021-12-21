1c3f90cc6deb563351f7742187a96d2b3424c166
david.gomez <david.garcia@neta.mx>
2021-12-21 18:39:03 -0300

    update

--- src/server/routes/category/getProducts.ts
+++ src/server/routes/category/getProducts.ts
@@ -28,7 +28,7 @@ export class getProductsByCategoryIdRoute {
      * @swagger
      * components:
      *  schemas:
-     *    CategoryBaseModel:
+     *    ProductBaseModel:
      *      type: object
      *      properties:
      *        Id:
@@ -46,12 +46,36 @@ export class getProductsByCategoryIdRoute {
      *        UpdatedOnUtc:
      *          type: Date
      *          description: The product update date in UTC format
+     *        Sku:
+     *          type: string
+     *          description: The identifier of the product
+     *        Price:
+     *          type: number
+     *          description: Current price of product
+     *        OldPrice:
+     *          type: number
+     *          description: Old price of product
+     *        CostPrice:
+     *          type: number
+     *          description: Cost price of product
+     *        OrderMinimumQuantity:
+     *          type: number
+     *          description: Minimum product quantity
+     *        OrderMaximumQuantity:
+     *          type: number
+     *          description: Maximum product quantity
      *      example:
-     *        Id: 1
-     *        Name: Limpieza del Hogar
-     *        SeoFilename: 0
-     *        CreatedOnUtc: 2021-07-22T23:10:59.000Z
-     *        UpdatedOnUtc: 2021-10-19T19:18:59.000Z
+     *         Id: 1804
+     *         Name: Shampoo Head & Shoulders alivio refrescante 375 ml
+     *         SeoFilename: https://testinglab.netamx.app/images/thumbs/0001804_undefined_415.jpeg,
+     *         CreatedOnUtc: 2021-11-12T20:56:13.000Z
+     *         UpdatedOnUtc: 2021-12-01T21:31:38.000Z
+     *         Sku: 10076
+     *         Price: 45.9000
+     *         OldPrice: 55.0000
+     *         CostPrice: 0
+     *         OrderMinimumQuantity: 1
+     *         OrderMaximumQuantity: 30
      *    CategoryNotFound:
      *      type: object
      *      properties:
@@ -74,23 +98,25 @@ export class getProductsByCategoryIdRoute {
      * @swagger
      * tags:
      *  name: Category
-     *  description: Category endpoint
+     *  description: Product Category endpoint
      */
     /**
      * @swagger
-     * /category/{id}:
+     * /category/{id}/product:
      *  get:
-     *    summary: Get a category by Id
+     *    summary: Get a products for specific category
      *    tags: [Category]
      *    parameters:
      *      - $ref: '#/components/parameters/categoryId'
      *    responses:
      *      200:
-     *        description: The found Category
+     *        description: The list of categories
      *        content:
      *          application/json:
      *            schema:
-     *            $ref: '#/components/schemas/CategoryBaseModel'
+     *              type: array
+     *              items:
+     *                $ref: '#/components/schemas/ProductBaseModel'
      *      404:
      *        description: The category was not found
      *        content:
